f7fd0eb124a51c4317eadfc08cd40041830ac0d6
Ashish Suthar <asissuthar@gmail.com>
2020-02-24 19:11:35 +0530

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,22 +3,40 @@
 namespace Msg91\Tests;
 
 use Msg91\OtpClient;
+use Msg91\SmsClient;
 use PHPUnit\Framework\TestCase as BaseTestCase;
 
 class TestCase extends BaseTestCase
 {
     private $otpClient;
+    private $smsClient;
 
     protected function setUp(): void
     {
         $authKey = "";
+
         $templateId = "";
         $this->otpClient = new OtpClient($authKey, $templateId);
+
+        $sender = "";
+        $this->smsClient = new SmsClient($authKey, $sender);
+
     }
 
-    public function testClient(): void
+    public function testOtpClient(): void
     {
         $response = $this->otpClient->send();
+        $this->assertSame(200, $response->getStatusCode());
+    }
+
+    public function testSmsClient(): void
+    {
+        $response = $this->smsClient
+            ->setCountry("")
+            ->setRoute("")
+            ->setTo("")
+            ->setMessage("")
+            ->send();
         $this->assertSame(200, $response->getStatusCode());
     }
 }
